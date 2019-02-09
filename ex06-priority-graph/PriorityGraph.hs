0a7429e491e7b0bd01c1785837f3611ec2aec65b
Nezuo <natsume.x.rin@gmail.com>
2019-02-10 03:47:43 +0900

    Implement calculating effective priority

--- ex06-priority-graph/PriorityGraph.hs
+++ ex06-priority-graph/PriorityGraph.hs
@@ -1,7 +1,8 @@
 module PriorityGraph (
   PriorityGraph (..),
   setParent,
-  addParent
+  addParent,
+  priority
   ) where
 
 type Priority = Double
@@ -9,12 +10,26 @@ type Parent a = (PriorityGraph a, Priority)
 data PriorityGraph a = PriorityGraph (a, [Parent a]) | Root (a, Priority)
   deriving (Eq, Show)
 
+-- TODO: switch to sets
+-- TODO: prevent cycles
+-- TODO: possibly make it a monad
+
 setParent :: PriorityGraph a -> PriorityGraph a -> PriorityGraph a
-setParent (Root (c, pc)) p =
-  PriorityGraph (c, [(p, pc)])
+setParent (Root (c, pc)) p = PriorityGraph (c, [(p, pc)])
+-- TODO: setParent for PriorityGraph (replace parents)
 setParent a _ = a -- dummy for now
 
 addParent :: PriorityGraph a -> Parent a -> PriorityGraph a
 addParent (PriorityGraph (t, [])) p = PriorityGraph (t, [p])
 addParent (PriorityGraph (t, ps)) p = PriorityGraph (t, p:ps)
 addParent (Root (t, _)) p = PriorityGraph (t, [p])
+
+-- TODO: delete parent
+
+priority' :: (PriorityGraph a, Priority) -> Priority
+priority' (Root (_, pr), p) = p * pr
+priority' (pp, p) = p * priority pp
+
+priority :: PriorityGraph a -> Priority
+priority (Root (_, p)) = p
+priority (PriorityGraph (a, pp)) = sum $ map priority' pp
--- ex06-priority-graph/priorityGraphTest.hs
+++ ex06-priority-graph/priorityGraphTest.hs
@@ -4,17 +4,20 @@ import PriorityGraph
 tests = TestList [
   setsParentToRootTask,
   addsRootParentToNonRootTask,
-  addParentWorksOnRootTaskDiscartingOriginalPriority
+  addParentWorksOnRootTaskDiscardingOriginalPriority,
+  effectivePriorityOfRootTaskIsItsPriority,
+  effectivePriorityOfChildTask
                  ]
+
+
+rootA5 = Root ("A", 5)
+rootB1 = Root ("B", 1)
+childCA3 = PriorityGraph ("C", [(rootA5, 3)])
 
 
 setsParentToRootTask =
   setParent (Root ("child", 5)) (Root ("parent", 1)) ~?=
     PriorityGraph ("child", [(Root ("parent", 1), 5)])
-
-rootA5 = Root ("A", 5)
-rootB1 = Root ("B", 1)
-childCA3 = PriorityGraph ("C", [(rootA5, 3)])
 
 addsRootParentToNonRootTask = 
   addParent childCA3 (rootB1, 2) ~?=
@@ -24,5 +27,14 @@ addParentWorksOnRootTaskDiscardingOriginalPriority =
   addParent rootA5 (rootB1, 7) ~?=
     PriorityGraph ("A", [(rootB1, 7)])
 
+effectivePriorityOfRootTaskIsItsPriority =
+  priority rootA5 ~?= 5
+
+effectivePriorityOfChildTask = TestList [
+  priority (PriorityGraph ("test", [])) ~?= 0,
+  priority childCA3 ~?= 15,
+  priority (addParent rootB1 (childCA3, 2)) ~?= 30,
+  priority (addParent childCA3 (rootB1, 2)) ~?= 17
+                                        ]
 
 main = runTestTT tests
