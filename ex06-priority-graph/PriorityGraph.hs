e5188f80090854cf2b86fee14dae25d140022d04
Nezuo <natsume.x.rin@gmail.com>
2019-02-10 01:25:38 +0900

    Implement parenting

--- ex06-priority-graph/PriorityGraph.hs
+++ ex06-priority-graph/PriorityGraph.hs
@@ -1,5 +1,6 @@
 module PriorityGraph (
   PriorityGraph (..),
+  setParent,
   addParent
   ) where
 
@@ -8,8 +9,12 @@ type Parent a = (PriorityGraph a, Priority)
 data PriorityGraph a = PriorityGraph (a, [Parent a]) | Root (a, Priority)
   deriving (Eq, Show)
 
-addParent :: PriorityGraph a -> PriorityGraph a -> PriorityGraph a
-addParent (Root (c, pc)) p =
+setParent :: PriorityGraph a -> PriorityGraph a -> PriorityGraph a
+setParent (Root (c, pc)) p =
   PriorityGraph (c, [(p, pc)])
-addParent a _ = a -- dummy for now
+setParent a _ = a -- dummy for now
 
+addParent :: PriorityGraph a -> Parent a -> PriorityGraph a
+addParent (PriorityGraph (t, [])) p = PriorityGraph (t, [p])
+addParent (PriorityGraph (t, ps)) p = PriorityGraph (t, p:ps)
+addParent (Root (t, _)) p = PriorityGraph (t, [p])
--- ex06-priority-graph/priorityGraphTest.hs
+++ ex06-priority-graph/priorityGraphTest.hs
@@ -2,12 +2,27 @@ import Test.HUnit
 import PriorityGraph
 
 tests = TestList [
-  addsParentToTask
+  setsParentToRootTask,
+  addsRootParentToNonRootTask,
+  addParentWorksOnRootTaskDiscartingOriginalPriority
                  ]
 
 
-addsParentToTask =
-  addParent (Root ("child", 5)) (Root ("parent", 1)) ~?=
+setsParentToRootTask =
+  setParent (Root ("child", 5)) (Root ("parent", 1)) ~?=
     PriorityGraph ("child", [(Root ("parent", 1), 5)])
+
+rootA5 = Root ("A", 5)
+rootB1 = Root ("B", 1)
+childCA3 = PriorityGraph ("C", [(rootA5, 3)])
+
+addsRootParentToNonRootTask = 
+  addParent childCA3 (rootB1, 2) ~?=
+    PriorityGraph ("C", [(rootB1, 2), (rootA5, 3)])
+
+addParentWorksOnRootTaskDiscartingOriginalPriority =
+  addParent rootA5 (rootB1, 7) ~?=
+    PriorityGraph ("A", [(rootB1, 7)])
+
 
 main = runTestTT tests
