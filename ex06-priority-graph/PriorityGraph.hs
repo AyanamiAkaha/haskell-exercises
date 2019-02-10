3144c1afc0b72f572ca19180733051cd52180aa1
Nezuo <natsume.x.rin@gmail.com>
2019-02-10 21:35:54 +0900

    Cleanup - remove unncessary wrapping n tuple

--- ex06-priority-graph/PriorityGraph.hs
+++ ex06-priority-graph/PriorityGraph.hs
@@ -8,11 +8,11 @@ module PriorityGraph (
 
 type Priority = Double
 type Parent a = (PriorityGraph a, Priority)
-data PriorityGraph a = PriorityGraph (a, [Parent a]) | Root (a, Priority)
+data PriorityGraph a = Node a [Parent a] | Root a Priority
   deriving (Eq, Show)
 
 mapParent' :: (a -> b) -> (PriorityGraph a, Priority) -> (PriorityGraph b, Priority)
-mapParent' f (Root (a, pr), p) = (Root ((f a), pr), p)
+mapParent' f (Root a pr, p) = (Root (f a) pr, p)
 mapParent' f (g, p) = (fmap f g, p)
 
 mapParent :: (a -> b) -> [(PriorityGraph a, Priority)] -> [(PriorityGraph b, Priority)]
@@ -24,37 +24,37 @@ getTasks' [] = []
 getTasks' ((t,_):pp) = (getTasks t) ++ (getTasks' pp)
 
 getTasks :: PriorityGraph a -> [a]
-getTasks (Root (t, _)) = [t]
-getTasks (PriorityGraph (t, ps)) = t:(getTasks' ps)
+getTasks (Root t _) = [t]
+getTasks (Node t ps) = t:(getTasks' ps)
 
 instance Functor PriorityGraph where
-  fmap f (Root (t, p)) = Root (f t, p)
-  fmap f (PriorityGraph (t, pp)) = PriorityGraph (f t, mapParent f pp)
+  fmap f (Root t p) = Root (f t) p
+  fmap f (Node t pp) = Node (f t) (mapParent f pp)
 
 instance Applicative PriorityGraph where
-  pure a = Root (a, 1)
-  Root (f, p1) <*> pg = fmap f pg
+  pure a = Root a 1
+  Root f p1 <*> pg = fmap f pg
 --  PriorityGraph (f, fs) <*> PriorityGraph (t, ts) = _
 
 -- TODO: prevent cycles
 -- TODO: possibly make it a monad
 
 setParent :: PriorityGraph a -> PriorityGraph a -> PriorityGraph a
-setParent (Root (c, pc)) p = PriorityGraph (c, [(p, pc)])
+setParent (Root c pc) p = Node c [(p, pc)]
 -- TODO: setParent for PriorityGraph (replace parents)
 setParent a _ = a -- dummy for now
 
 addParent :: PriorityGraph a -> Parent a -> PriorityGraph a
-addParent (PriorityGraph (t, [])) p = PriorityGraph (t, [p])
-addParent (PriorityGraph (t, ps)) p = PriorityGraph (t, p:ps)
-addParent (Root (t, _)) p = PriorityGraph (t, [p])
+addParent (Root t _) p = Node t [p]
+addParent (Node t []) p = Node t [p]
+addParent (Node t ps) p = Node t (p:ps)
 
 -- TODO: delete parent
 
 priority' :: (PriorityGraph a, Priority) -> Priority
-priority' (Root (_, pr), p) = p * pr
+priority' (Root _ pr, p) = p * pr
 priority' (pp, p) = p * priority pp
 
 priority :: PriorityGraph a -> Priority
-priority (Root (_, p)) = p
-priority (PriorityGraph (a, pp)) = sum $ map priority' pp
+priority (Root _ p) = p
+priority (Node a pp) = sum $ map priority' pp
--- ex06-priority-graph/priorityGraphTest.hs
+++ ex06-priority-graph/priorityGraphTest.hs
@@ -16,47 +16,50 @@ tests = TestList [
                  ]
 
 
-rootA5 = Root ("A", 5)
-rootB1 = Root ("B", 1)
-childCA3 = PriorityGraph ("C", [(rootA5, 3)])
-childDC7B1 = PriorityGraph ("D", [(childCA3, 7), (rootB1, 1)])
+rootA5 = Root "A" 5
+rootB1 = Root "B" 1
+childCA3 = Node "C" [(rootA5, 3)]
+childDC7B1 = Node "D" [(childCA3, 7), (rootB1, 1)]
 
 
 setsParentToRootTask =
-  setParent (Root ("child", 5)) (Root ("parent", 1)) ~?=
-    PriorityGraph ("child", [(Root ("parent", 1), 5)])
+  setParent (Root "child" 5) (Root "parent" 1) ~?=
+    Node "child" [(Root "parent" 1, 5)]
 
 addsRootParentToNonRootTask = 
   addParent childCA3 (rootB1, 2) ~?=
-    PriorityGraph ("C", [(rootB1, 2), (rootA5, 3)])
+    Node "C" [(rootB1, 2), (rootA5, 3)]
 
 addParentWorksOnRootTaskDiscardingOriginalPriority =
   addParent rootA5 (rootB1, 7) ~?=
-    PriorityGraph ("A", [(rootB1, 7)])
+    Node "A" [(rootB1, 7)]
 
 effectivePriorityOfRootTaskIsItsPriority =
   priority rootA5 ~?= 5
 
 effectivePriorityOfChildTask = TestList [
-  priority (PriorityGraph ("test", [])) ~?= 0,
+  priority (Node "test" []) ~?= 0,
   priority childCA3 ~?= 15,
   priority (addParent rootB1 (childCA3, 2)) ~?= 30,
   priority (addParent childCA3 (rootB1, 2)) ~?= 17
                                         ]
 
 mapsOverRoot =
-  fmap (" "++) rootA5 ~?= Root (" A", 5)
+  fmap (" "++) rootA5 ~?= Root " A" 5
 
 mapsOverTaskWithSingleParent =
-  fmap (" "++) childCA3 ~?= PriorityGraph (" C", [(Root (" A", 5), 3)])
+  fmap (" "++) childCA3 ~?= Node " C" [(Root " A" 5, 3)]
 
 mapsOverTasksWithMultipleParents =
   (fmap (" "++) $ addParent childCA3 (rootB1, 2)) ~?=
-    PriorityGraph (" C", [(Root (" B", 1), 2), (Root (" A", 5), 3)])
+    Node " C" [(Root " B" 1, 2), (Root " A" 5, 3)]
 
 mapsOverNestedParenting =
   fmap (" "++) childDC7B1 ~?=
-    PriorityGraph (" D", [(PriorityGraph (" C", [(Root (" A", 5), 3)]), 7), (Root (" B", 1), 1)])
+    Node " D" [
+                (Node " C" [(Root " A" 5, 3)], 7),
+                (Root " B" 1, 1)
+              ]
 
 getsTaskListFromTree =
   getTasks childDC7B1 \\ ["D", "C", "B", "A"] ~?= []
